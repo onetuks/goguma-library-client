c6e869ffa4eb4cfc5ab9b25aac72d431e97860d8
onetuks <onetuks@naver.com>
2024-08-05 22:04:16 +0900

    Feat: 구독하기 버튼 컴포넌트 구현

---
+++ src/components/button/SubscribeButton.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    originSubscriptionStatus: boolean;
+  }>(),
+  {
+    originSubscriptionStatus: false,
+  }
+);
+
+let isSubscribed: boolean = ref(props.originSubscriptionStatus).value;
+
+const toggleSubscription = () => {
+  isSubscribed = !isSubscribed;
+};
+</script>
+
+<template>
+  <button
+    :class="['subscription-button', { subscribed: isSubscribed }]"
+    @click="toggleSubscription"
+  >
+    {{ isSubscribed ? "구독중" : "구독" }}
+  </button>
+</template>
+
+<style scoped>
+.subscription-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: transparent;
+  color: #756357; /* 피그마의 ivory/100 */
+  border: 2px solid #e8e6d5; /* 피그마의 ivory/600 */
+  border-radius: 5px;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.subscription-button.subscribed {
+  background-color: #756357; /* 피그마의 ivory/100 */
+  color: white;
+}
+
+.subscription-button:not(.subscribed):hover {
+  background-color: #e8e6d5; /* 피그마의 ivory/600 */
+  color: #756357; /* 피그마의 ivory/100 */
+}
+</style>
