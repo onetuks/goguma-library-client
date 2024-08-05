4373fc42361d57f10a831a2c966bf0624835483c
onetuks <onetuks@naver.com>
2024-08-05 21:56:02 +0900

    Feat: 서평쓰기 버튼 컴포넌트 구현

---
+++ src/components/button/ReviewRegisterButton.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import axios from "axios";
+
+interface Review {
+  memberId: number;
+  bookId: number;
+  reviewTitle: string;
+  reviewContent: string;
+}
+
+const registerReview = (review: Review): void => {
+  const response = axios.post("http://localhost:8080/api/reviews", review);
+  console.log(response);
+};
+</script>
+
+<template>
+  <div class="registration-wrapper">
+    <button class="registration-button" @click="registerReview">
+      서평 쓰기
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.registration-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 130px;
+  height: 30px;
+}
+
+.registration-button {
+  color: var(--black);
+  background-color: transparent;
+}
+</style>
