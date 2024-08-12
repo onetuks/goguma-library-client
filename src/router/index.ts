1f1879805ca5c5553edd71d23d51f7843529a847
MIchaeL <kook3553@gmail.com>
2024-08-12 17:22:37 +0900

    WIP on page/book

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import SamplePage from "@/pages/SamplePage.vue";
 import BookInformationPage from "@/pages/BookInformationPage.vue";
-import BookInformationPage from "@/pages/BookInformationPage.vue";
 import LoginHandlerView from "@/pages/login/handler/LoginHandlerView.vue";
+import LoginPage from "@/pages/login/LoginPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", component: SamplePage },
   { path: "/sample", component: SamplePage },
-  { path: "/bookinformationpage", component: BookInformationPage },
   { path: "/bookinformationpage", component: BookInformationPage },
   { path: "/login", component: LoginPage },
   { path: "/login/oauth2/callback/:provider", component: LoginHandlerView },
