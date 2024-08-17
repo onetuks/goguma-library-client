234f9fcc7db3a82083cdd433d5ac782790476a62
onetuks <onetuks@naver.com>
2024-08-17 16:16:37 +0900

    Feat: 멤버 서버 API 요청 기능 구현

---
+++ src/api/MemberApis.ts
@@ -0,0 +1,59 @@
+import { Category } from "@/types/Category";
+import { get, patchFormData } from "@/api/ServerRequest";
+
+export interface MemberPatchRequest {
+  nickname: string;
+  introduction: string;
+  interestedCategories: Category[];
+  isAlarmAccepted: boolean;
+}
+
+export interface MemberResponse {
+  memberId: number;
+  nickname: string;
+  introduction: string;
+  interestedCategories: Category[];
+  isAlarmAccepted: boolean;
+  points: number;
+  profileImageUrl: string;
+  profileBackgroundImageUrl: string;
+  memberStatics: MemberStatics;
+}
+
+export interface MemberStatics {
+  memberStaticsId: number;
+  reviewCounts: number;
+  followerCounts: number;
+  followingCounts: number;
+  reviewCategoryCounts: Map<Category, number>;
+}
+
+export const MemberApis = {
+  URI_PREFIX: "/members",
+  patchMemberProfile: async (
+    memberId: number,
+    request: MemberPatchRequest,
+    profileImage?: File,
+    profileBackgroundImage?: File
+  ): Promise<MemberResponse> => {
+    // 멤버 프로필 수정
+    return await patchFormData(
+      `${MemberApis.URI_PREFIX}/${memberId}`,
+      request,
+      ["profile-image", profileImage],
+      ["profile-background-image", profileBackgroundImage]
+    ).then((data) => data as MemberResponse);
+  },
+  getMemberProfile: async (memberId: number): Promise<MemberResponse> => {
+    // 멤버 프로필 단건 조회
+    return await get(`${MemberApis.URI_PREFIX}/${memberId}`).then(
+      (data) => data as MemberResponse
+    );
+  },
+  getRecommendedMemberProfiles: async (): Promise<MemberResponse[]> => {
+    // 추천 멤버 프로필 조회
+    return await get(`${MemberApis.URI_PREFIX}/recommend`).then(
+      (data) => data as MemberResponse[]
+    );
+  },
+};
