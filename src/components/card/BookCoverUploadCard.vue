b12059f1ac3fa419f380b0a442769ac691eb4e85
onetuks <onetuks@naver.com>
2024-08-22 15:40:25 +0900

    Refactor: 도서 커버 업로더 컴포넌트 리팩토링

--- src/components/card/BookCoverUploadCard.vue
+++ src/components/card/BookCoverUploadCard.vue
@@ -1,58 +1,92 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
-const fileInput = ref<HTMLInputElement | null>(null);
-const coverUrl = ref<string | undefined>(undefined);
+const emits = defineEmits<{
+  (event: "update:CoverImageFile", file: File | null): void;
+  (event: "update:CoverImageFilename", filename: string | null): void;
+}>();
 
-const hasCover = (): boolean => coverUrl.value !== undefined;
+const coverImageUrl = ref<string | null>(null); // 이미지 미리보기 URL
+const coverImageFile = ref<File | null>(null); // 실제 파일
+const coverImageFilename = ref<string | null>(null); // 서버 요청에 제공할 파일명
+const coverImageInput = ref<HTMLInputElement | null>(null); // 파일 선택 input
 
-const changeFile = (e: Event): void => {
+watch(coverImageFile, (newCoverImageFile) => {
+  emits("update:CoverImageFile", newCoverImageFile);
+});
+
+watch(coverImageFilename, (newCoverImageFilename) => {
+  emits("update:CoverImageFilename", newCoverImageFilename);
+});
+
+const changeCoverImageFile = (e: Event): void => {
   const target = e.target as HTMLInputElement;
   const file = target.files?.[0];
   if (file) {
-    coverUrl.value = URL.createObjectURL(file);
+    const newUrl = URL.createObjectURL(file);
+    coverImageUrl.value = newUrl;
+    coverImageFile.value = file;
+    coverImageFilename.value =
+      newUrl.split("/").pop() + file.type.replace("image/", ".");
   }
 };
 
 const triggerChangeFileClick = (): void => {
-  fileInput.value?.click();
+  coverImageInput.value?.click();
 };
 
 const removeCover = (): void => {
-  coverUrl.value = undefined;
-  fileInput.value = null;
+  coverImageFilename.value = null;
+  coverImageUrl.value = null;
+  coverImageFile.value = null;
+  coverImageInput.value = null;
 };
 </script>
 
 <template>
-  <div class="book-cover-uploader" :class="{ 'has-cover': hasCover }">
-    <input
-      type="file"
-      @change="changeFile"
-      ref="fileInput"
-      style="display: none"
-    />
-    <div v-if="hasCover()" class="cover-preview">
-      <img :src="coverUrl" alt="Book Cover" class="book-cover" />
-      <img
-        src="../../assets/icon/upload/trashcan_icon.png"
-        alt="Remove Cover"
-        class="remove-button"
-        @click="removeCover"
+  <div class="book-cover-container">
+    <label class="form-label">표지이미지</label>
+    <div
+      class="book-cover-uploader"
+      :class="{ 'has-cover': coverImageFile !== null }"
+    >
+      <input
+        type="file"
+        accept="image/*"
+        @change="changeCoverImageFile"
+        ref="coverImageInput"
+        style="display: none"
       />
-    </div>
-    <div v-else class="upload-placeholder" @click="triggerChangeFileClick">
-      <img
-        src="../../assets/icon/upload/camera_icon.png"
-        alt="Camera Icon"
-        class="camera-icon"
-      />
-      <p>도서 표지를 첨부해주세요</p>
+
+      <div v-if="coverImageUrl" class="cover-preview">
+        <img :src="coverImageUrl" alt="Book Cover" class="book-cover" />
+        <img
+          src="@/assets/icon/upload/trashcan_icon.png"
+          alt="Remove Cover"
+          class="remove-button"
+          @click="removeCover"
+        />
+      </div>
+      <div v-else class="upload-placeholder" @click="triggerChangeFileClick">
+        <img
+          src="@/assets/icon/upload/camera_icon.png"
+          alt="Camera Icon"
+          class="camera-icon"
+        />
+        <p>도서 표지를 첨부해주세요</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.book-cover-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 10px;
+}
+
 .book-cover-uploader {
   background-color: #dcdcdc; /* 피그마의 gray/1000 */
   padding: 20px;
@@ -61,7 +95,7 @@ const removeCover = (): void => {
   border-radius: 5px;
   cursor: pointer;
   transition: border 0.3s ease;
-  width: 360px;
+  width: auto;
   height: 240px;
   display: flex;
   justify-content: center;
@@ -82,6 +116,12 @@ const removeCover = (): void => {
   width: 50px;
   height: 36px;
   margin-bottom: 10px;
+}
+
+.form-label {
+  font-family: var(--font-family-bold), sans-serif;
+  font-size: 16px;
+  text-align: left;
 }
 
 .upload-placeholder p {
