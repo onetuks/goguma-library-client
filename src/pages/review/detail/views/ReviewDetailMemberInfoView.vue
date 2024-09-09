a40cbe176ab88d04c652fd3ff799d53dd998fdb4
onetuks <onetuks@naver.com>
2024-09-10 03:48:50 +0900

    Feat: 멤버 닉네임 클릭 시 타인의 서재로 이동

--- src/pages/review/detail/views/ReviewDetailMemberInfoView.vue
+++ src/pages/review/detail/views/ReviewDetailMemberInfoView.vue
@@ -2,12 +2,17 @@
 import FollowButton from "@/components/button/FollowButton.vue";
 import { Member, MemberApis } from "@/api/MemberApis";
 import { ref } from "vue";
+import router from "@/router";
 
 const props = defineProps<{
   memberId: number;
 }>();
 
 const member = ref<Member>();
+
+const moveToOtherMemberStudy = (): void => {
+  router.push(`/study?member-id=${props.memberId}`);
+};
 
 const handleProfileImageError = (event: Event) => {
   const target = event.target as HTMLImageElement;
@@ -31,7 +36,7 @@ fetchMember();
       class="review-detail-member-profile-image"
       @error="handleProfileImageError"
     />
-    <div class="review-detail-member-nickname">
+    <div class="review-detail-member-nickname" @click="moveToOtherMemberStudy">
       {{ member.nickname }}
     </div>
     <FollowButton :member-id="member.memberId" />
@@ -57,5 +62,21 @@ fetchMember();
   color: var(--text-secondary);
   font-size: 14px;
   font-family: var(--font-family-bold), sans-serif;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  border-radius: 10px;
+  padding: 0 3px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+}
+
+.review-detail-member-nickname:hover {
+  background-color: var(--surface-fourth);
+  color: var(--text-fourth);
+}
+
+.review-detail-member-nickname:active {
+  background-color: var(--surface-sixth);
+  color: var(--text-tertiary);
 }
 </style>
